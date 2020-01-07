e791c6af1dd2d95c4ce2e7b0001083b86fcfdd20
chenallee <lchenalloy@gmail.com>
2020-01-07 17:17:58 -0500

    figuring out element targeting to save text to js

--- script.js
+++ script.js
@@ -1,30 +1,75 @@
 var $currentDay = $("#currentDay");
 var $timeBlocks = $(".time-block");
+var $scheduleArea = $(".schedule");
 
+var toDoItems = [];
+//each object has a hour property and a text property
+ 
 var currentDate = moment().format("dddd, MMMM Do");
 var currentHour = moment().format("H");
-console.log(currentHour);
-currentHour = 13;
 
+//display current date
 $currentDay.text(currentDate); 
 
+//add style to time blocks to show where we are in the day
 $timeBlocks.each(function(){
   var $thisBlock = $(this);
-  console.log($thisBlock.attr("data-hour"));
-  console.log(currentHour);
-  
-  if( $thisBlock.attr("data-hour") == currentHour){
+  var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
+
+  if( thisBlockHr == currentHour){
     $thisBlock.addClass("present").removeClass("past future");
   }
-  if ($thisBlock.attr("data-hour") < currentHour){
+  if (thisBlockHr < currentHour){
     $thisBlock.addClass("past").removeClass("present future");
   }
-  if ($thisBlock.attr("data-hour") > currentHour){
-    
+  if (thisBlockHr > currentHour){
     $thisBlock.addClass("future").removeClass("past present");
   }
 });
 
-// var thisMoment = moment().format();
-// console.log(moment());
-// console.log(thisMoment);
+/*var todos = localStorage.getItem("todos");
+var parsedTodos = JSON.parse(todos);
+console.log(parsedTodos);*/
+function initializeTodos(){
+  //just in case we want to update with more timeblock, i will not hardcode timeblocks num here
+  $timeBlocks.each(function(){
+    var $thisBlock = $(this);
+    var toDoObject = {};
+    toDoObject.hour = parseInt($thisBlock.attr("data-hour"));
+    toDoItems.push(toDoObject);
+  }); //this might not be the best bc this is will be set if theres nothing in local storage... but if there IS something in local storage.
+
+  localStorage.setItem("todos", JSON.stringify(toDoItems));
+  //console.log(toDoItems);
+}
+
+function renderSchedule(){
+
+}
+
+function saveHandler(){
+//event.preventDefault();
+if (event.target.matches("button")){
+
+  var hourToUpdate = event.target.parentElement.getAttribute("data-hour");
+  var itemToAdd = (($(event.target).parent()).children("textarea")).val();
+  //i was having issues bc i was mixing javascript selectors with jquery functions... lets fix his
+
+  //see which item we need to update based on the hour of the button clicked matching
+  for (var j = 0; j < toDoItems.length; j++){
+    if (toDoItems[j].hour == hourToUpdate){
+      //set its text to what was added to textarea
+      toDoItems[j].text = itemToAdd;
+    }
+  }
+}
+  //renderSchedule();
+  console.log(toDoItems);
+}
+
+//add event listener to buttons
+$().ready(function(){
+  initializeTodos();
+  $scheduleArea.click(saveHandler);
+  
+});
